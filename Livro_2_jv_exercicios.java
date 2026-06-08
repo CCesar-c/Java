1686335ad044faf6feffe6e496b8c0e0bccef710
CCesar-c <lianbastiam@gmail.com>
2026-06-08 09:58:45 -0300

    fix

---
+++ Livro_2_jv_exercicios.java
@@ -0,0 +1,90 @@
+public class Livro_2_jv_exercicios {
+
+    static void SalidaDatos(){
+
+        System.out.println("Hello world!!");
+        System.out.println("\033[37m sabanas !!");
+        System.out.println("\033[36m cielo !!");
+        System.out.println("\033[35m nazareno !!");
+        System.out.println("\033[34m mar !!");
+        System.out.println("\033[33m mandarina!!");
+        System.out.println("\033[32m hierba !!");
+        System.out.println("\033[31m tomate!!");
+
+        System.out.println("\u266A Corchea!!");
+        System.out.println("\u266B 2 Corchea!!");
+
+        System.out.printf("El numero %d no tiene decimales.", 21);
+        System.out.println("\n  ");
+        System.out.printf("El numero %f sale con decimales.", 21.0);
+        System.out.println("\n  ");
+        System.out.printf("El numero %.3f sale exactamento con 3 decimales.", 21.0);
+        System.out.println("\n ");
+        System.out.printf("La cadena de letras es %s \n", "Asi");
+
+        System.out.println("ATV");
+        System.out.print("\n");
+        double Num = 4898868.2954;
+        System.out.printf("Hola soy \033[36m%s \n", "Cesar..!!");
+        System.out.print("\n");
+        System.out.printf("\033[30m Vivo en %s \n mi numero es %.4f ", "Brasil, palhoca, Guarda de cubatao", Num);
+        System.out.println("\n\n\n\n\n");
+        System.out.println(" __________________________");
+        System.out.println(" * Ingles     -  spanish  *");
+        System.out.println(" 1. Computer  - Computadora");
+        System.out.println(" 2. Student   - Estudiante ");
+        System.out.println(" 3. Cat       - Gato       ");
+        System.out.println(" 4. Dog       - Perro      ");
+        System.out.println(" 5. Penguin   - Pinguino   ");
+        System.out.println(" 6. Telephone - Telefono   ");
+        System.out.println(" 7. Box       - Caja       ");
+        System.out.println(" 8. Circle    - Circulo    ");
+        System.out.println(" 9. Tree      - Arbol      ");
+        System.out.println(" 10. Black    - Negro      ");
+
+        System.out.println("Desde las \033[31m06:00 A.M\033[30m hasta las \033[31m11:00 P.M");
+        
+        
+        System.out.println("\033[30m \n");
+        System.out.println("    *    ");
+        System.out.println("   ***   ");
+        System.out.println("  *****  ");
+        System.out.println(" ******* ");
+        System.out.println("*********\n");
+
+        System.out.println("    *    ");
+        System.out.println("   * *   ");
+        System.out.println("  *   *  ");
+        System.out.println(" *     * ");
+        System.out.println("*********\n");
+        
+        System.out.println("*********");
+        System.out.println(" *     * ");
+        System.out.println("  *   *  ");
+        System.out.println("   * *   ");
+        System.out.println("    *  \n");
+        
+        
+        System.out.print("\033[30m");
+        System.out.print("\n___________");
+        System.out.print("\n|   _______\\");
+        System.out.print("\n|   |     | \\");
+        System.out.print("\n\033[36m|___|_____|__\\_________");
+        System.out.print("\n|   |     |            |");
+        System.out.print("\n|   |_____|            |");
+        System.out.print("\n|______________________|");
+        System.out.print("\033[30m");
+
+        byte a = 'a';
+        byte b = 'b';
+        byte c = 'c';
+        byte d = 'z';
+
+        System.out.print(" a + b + c + d: " + a + b + c + d);
+
+    }
+
+    public static void main(String[] args) {
+        SalidaDatos();
+    }
+}

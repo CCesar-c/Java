b6923b86d6f72e707b9d665d46883c6f53603267
CCesar-c <cesarinfluencer14@gmail.com>
2026-06-10 07:50:35 -0300

    tarea 8 variables

--- Livro_2_jv_exercicios.java
+++ Livro_2_jv_exercicios.java
@@ -2,86 +2,121 @@ public class Livro_2_jv_exercicios {
 
     static void SalidaDatos(){
 
-        System.out.println("Hello world!!");
-        System.out.println("\033[37m sabanas !!");
-        System.out.println("\033[36m cielo !!");
-        System.out.println("\033[35m nazareno !!");
-        System.out.println("\033[34m mar !!");
-        System.out.println("\033[33m mandarina!!");
-        System.out.println("\033[32m hierba !!");
-        System.out.println("\033[31m tomate!!");
+        // System.out.println("Hello world!!");
+        // System.out.println("\033[37m sabanas !!");
+        // System.out.println("\033[36m cielo !!");
+        // System.out.println("\033[35m nazareno !!");
+        // System.out.println("\033[34m mar !!");
+        // System.out.println("\033[33m mandarina!!");
+        // System.out.println("\033[32m hierba !!");
+        // System.out.println("\033[31m tomate!!");
 
-        System.out.println("\u266A Corchea!!");
-        System.out.println("\u266B 2 Corchea!!");
+        // System.out.println("\u266A Corchea!!");
+        // System.out.println("\u266B 2 Corchea!!");
 
-        System.out.printf("El numero %d no tiene decimales.", 21);
-        System.out.println("\n  ");
-        System.out.printf("El numero %f sale con decimales.", 21.0);
-        System.out.println("\n  ");
-        System.out.printf("El numero %.3f sale exactamento con 3 decimales.", 21.0);
-        System.out.println("\n ");
-        System.out.printf("La cadena de letras es %s \n", "Asi");
+        // System.out.printf("El numero %d no tiene decimales.", 21);
+        // System.out.println("\n  ");
+        // System.out.printf("El numero %f sale con decimales.", 21.0);
+        // System.out.println("\n  ");
+        // System.out.printf("El numero %.3f sale exactamento con 3 decimales.", 21.0);
+        // System.out.println("\n ");
+        // System.out.printf("La cadena de letras es %s \n", "Asi");
 
-        System.out.println("ATV");
-        System.out.print("\n");
-        double Num = 4898868.2954;
-        System.out.printf("Hola soy \033[36m%s \n", "Cesar..!!");
-        System.out.print("\n");
-        System.out.printf("\033[30m Vivo en %s \n mi numero es %.4f ", "Brasil, palhoca, Guarda de cubatao", Num);
-        System.out.println("\n\n\n\n\n");
-        System.out.println(" __________________________");
-        System.out.println(" * Ingles     -  spanish  *");
-        System.out.println(" 1. Computer  - Computadora");
-        System.out.println(" 2. Student   - Estudiante ");
-        System.out.println(" 3. Cat       - Gato       ");
-        System.out.println(" 4. Dog       - Perro      ");
-        System.out.println(" 5. Penguin   - Pinguino   ");
-        System.out.println(" 6. Telephone - Telefono   ");
-        System.out.println(" 7. Box       - Caja       ");
-        System.out.println(" 8. Circle    - Circulo    ");
-        System.out.println(" 9. Tree      - Arbol      ");
-        System.out.println(" 10. Black    - Negro      ");
+        // System.out.println("ATV");
+        // System.out.print("\n");
+        // double Num = 4898868.2954;
+        // System.out.printf("Hola soy \033[36m%s \n", "Cesar..!!");
+        // System.out.print("\n");
+        // System.out.printf("\033[30m Vivo en %s \n mi numero es %.4f ", "Brasil, palhoca, Guarda de cubatao", Num);
+        // System.out.println("\n\n\n\n\n");
+        // System.out.println(" __________________________");
+        // System.out.println(" * Ingles     -  spanish  *");
+        // System.out.println(" 1. Computer  - Computadora");
+        // System.out.println(" 2. Student   - Estudiante ");
+        // System.out.println(" 3. Cat       - Gato       ");
+        // System.out.println(" 4. Dog       - Perro      ");
+        // System.out.println(" 5. Penguin   - Pinguino   ");
+        // System.out.println(" 6. Telephone - Telefono   ");
+        // System.out.println(" 7. Box       - Caja       ");
+        // System.out.println(" 8. Circle    - Circulo    ");
+        // System.out.println(" 9. Tree      - Arbol      ");
+        // System.out.println(" 10. Black    - Negro      ");
 
-        System.out.println("Desde las \033[31m06:00 A.M\033[30m hasta las \033[31m11:00 P.M");
+        // System.out.println("Desde las \033[31m06:00 A.M\033[30m hasta las \033[31m11:00 P.M");
         
         
-        System.out.println("\033[30m \n");
-        System.out.println("    *    ");
-        System.out.println("   ***   ");
-        System.out.println("  *****  ");
-        System.out.println(" ******* ");
-        System.out.println("*********\n");
+        // System.out.println("\033[30m \n");
+        // System.out.println("    *    ");
+        // System.out.println("   ***   ");
+        // System.out.println("  *****  ");
+        // System.out.println(" ******* ");
+        // System.out.println("*********\n");
 
-        System.out.println("    *    ");
-        System.out.println("   * *   ");
-        System.out.println("  *   *  ");
-        System.out.println(" *     * ");
-        System.out.println("*********\n");
+        // System.out.println("    *    ");
+        // System.out.println("   * *   ");
+        // System.out.println("  *   *  ");
+        // System.out.println(" *     * ");
+        // System.out.println("*********\n");
         
-        System.out.println("*********");
-        System.out.println(" *     * ");
-        System.out.println("  *   *  ");
-        System.out.println("   * *   ");
-        System.out.println("    *  \n");
+        // System.out.println("*********");
+        // System.out.println(" *     * ");
+        // System.out.println("  *   *  ");
+        // System.out.println("   * *   ");
+        // System.out.println("    *  \n");
         
         
-        System.out.print("\033[30m");
-        System.out.print("\n___________");
-        System.out.print("\n|   _______\\");
-        System.out.print("\n|   |     | \\");
-        System.out.print("\n\033[36m|___|_____|__\\_________");
-        System.out.print("\n|   |     |            |");
-        System.out.print("\n|   |_____|            |");
-        System.out.print("\n|______________________|");
-        System.out.print("\033[30m");
+        // System.out.print("\033[30m");
+        // System.out.print("\n___________");
+        // System.out.print("\n|   _______\\");
+        // System.out.print("\n|   |     | \\");
+        // System.out.print("\n\033[36m|___|_____|__\\_________");
+        // System.out.print("\n|   |     |            |");
+        // System.out.print("\n|   |_____|            |");
+        // System.out.print("\n|______________________|");
 
-        byte a = 'a';
-        byte b = 'b';
-        byte c = 'c';
-        byte d = 'z';
+        // byte a = 'a';
+        // byte b = 'b';
+        // byte c = 'c';
+        // byte d = 'z';
 
-        System.out.print(" a + b + c + d: " + a + b + c + d);
+        // System.out.print(" a + b + c + d: " + a + b + c + d);
+      // int x = 144, y = 999;
 
+      // System.out.println(x + y);
+      // System.out.println(x - y);
+      // System.out.println(x * y);
+      // System.out.println(x / y);
+
+      // String nombre = "Cesar";
+      // String direccion = "Rua servidao Medeiros";
+      // long numero = 48988682954l;
+      // System.out.println("mi nombre es: " + nombre + "\n Vivo en: " + direccion + "\n y mi numero: " + numero);
+
+      // euros ==> pesetas
+      // int euros = 10;
+      // double pesetas = 166.386;
+      // float resultado = euros * pesetas;
+      // System.out.println("resultado de " + resultado);
+
+      //pesetas ==> euros
+      // double pesetas = 10.00;
+      // float euros = 0.006f;
+      // float resultado = pesetas * euros;
+      // System.out.println( pesetas * euros );
+
+
+      // String enunciado = "mi nombre es cesa";
+      // char complemento = 'R';
+      // System.out.println(enunciado + complemento);
+
+      char a = 'c';
+      char b = 'e';
+      char c = 's';
+      char d = 'a';
+      char e = 'r';
+      
+      String conjunto = String.;
+      System.out.println(conjunto);
     }
 
     public static void main(String[] args) {

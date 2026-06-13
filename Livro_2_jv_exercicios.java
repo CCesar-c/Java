559c6a8f98516469be2f56028d9198509f65f84c
CCesar-c <lianbastiam@gmail.com>
2026-06-13 17:04:15 -0300

    fixed

--- Livro_2_jv_exercicios.java
+++ Livro_2_jv_exercicios.java
@@ -192,124 +192,147 @@ public class Livro_2_jv_exercicios {
 
         // System.out.println("Horas trabajadas...");
         // int hrsTrabajadas = scan.nextInt();
-        
+
         // System.out.println("Segundo numero..." + (12 * hrsTrabajadas));
-        
+
         // Scanner scan = new Scanner(System.in);
-        
+
         // System.out.println("Radio...");
         // int radio = scan.nextInt();
         // System.out.println("height...");
         // int height = scan.nextInt();
-        // System.out.println("El volumen del cono:\n" + (Math.PI * (radio * radio) * height )/ 3);
+        // System.out.println("El volumen del cono:\n" + (Math.PI * (radio * radio) *
+        // height )/ 3);
 
         // Scanner scan = new Scanner(System.in);
-        
+
         // System.out.println("MB...");
         // int mb = scan.nextInt();
         // System.out.println("mb en kb..." + ( mb * 1000) + "kb");
-        
+
         // Scanner scan = new Scanner(System.in);
-        
+
         // System.out.println("KB...");
         // int kb = scan.nextInt();
         // System.out.println("kb en mb..." + ( kb / 1000) + "mb");
+
+        // Scanner scan = new Scanner(System.in);
+
+        // System.out.println("Primer examen...");
+        // int nota1 = scan.nextInt();
+        // System.out.println("¿Qué nota quieres sacar en el trimestre?");
+        // int nota2 = scan.nextInt();
+        // float ciemp = (nota2 - (nota1 * 0.40f) / 0.60f);
+        // System.out.println("resultado" + ciemp);
+
+        // String dia_semana = System.console().readLine();
+        // switch(dia_semana){
+        // case "lunes":
+        // System.out.println("gestão da qualidade ");
+        // break;
+        // case "martes":
+        // System.out.println("Edu cacion fisica ");
+        // break;
+        // case "miercoles":
+        // System.out.println("Artes ");
+        // break;
+        // case "jueves":
+        // System.out.println("Portuges ");
+        // break;
+        // case "viernes":
+        // System.out.println("Matematica");
+        // break;
+        // default:
+        // System.out.println("Nao e um dia da semana");
+        // break;
+        // }
+        // Scanner scan = new Scanner(System.in );
+        // int hrs = scan.nextInt();
+        // if(hrs >= 6 && hrs <= 12){
+        // System.out.println ("Buenos dias");
+        // }else if(hrs >= 13 && hrs <= 20){
+        // System.out.println("Buenas tardes");
+        // }else if(hrs >= 21 && hrs <= 5){
+        // System.out.println("Buenos noches");
+        // }else{
+        // System.out.println("horario errado")
+        // }
+        // Scanner scan = new Scanner(System.in );
+        // System.out.println("pon algun numero");
+        // int dia_semana = scan.nextInt();
+        // switch(dia_semana){
+        // case 1:
+        // System.out.println("lunes");
+        // break;
+        // case 2:
+        // System.out.println("martes");
+        // break;
+        // case 3:
+        // System.out.println("miercoles ");
+        // break;
+        // case 4:
+        // System.out.println("jueves");
+        // break;
+        // case 5:
+        // System.out.println("viernes");
+        // break;
+        // case 6:
+        // System.out.println("Sábado");
+        // break;
+        // case 7:
+        // System.out.println("Domingos");
+        // break;
+        // default:
+        // System.out.println("Coño");
+        // break;
+        // }
+        // Scanner scan = new Scanner(System.in );
+        // System.out.println("pon algun tus horas trabajadas");
+        // int hrsTrabajadas = scan.nextInt();
+        // if(hrsTrabajadas >= 41){
+        // int horasExtras = hrsTrabajadas - 40;
+        // System.out.println("Tu salário con horas extra: " + (horasExtras * 16 + 40 *
+        // 12));
+        // }else{
+        // System.out.println("Tu salario es de: " +(hrsTrabajadas * 12));
+        // }
+
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Usando la formula ax + b = 0");
+        System.out.println("di el numer 'A' ");
+        float a = scan.nextFloat();
+
+        System.out.println("di el numer 'B' ");
+        float b = scan.nextFloat();
+
+        float resposta = 0 - b;
+        float respostaFinal = resposta / a;
+
+        System.out.println("entoces: \nx = " + (respostaFinal));
+
+
+        // Scanner scanner = new Scanner(System.in);
+
+        // System.out.println("La altura del objeto");
+        // float height = scanner.nextFloat();
+        // float res = height / 9.81f;
         
-      //   Scanner scan = new Scanner(System.in);
-        
-      //   System.out.println("Primer examen...");
-      //   int nota1 = scan.nextInt();
-      //   System.out.println("¿Qué nota quieres sacar en el trimestre?");
-      //   int nota2 = scan.nextInt();
-      // float ciemp = (nota2 - (nota1 * 0.40f) / 0.60f);
-      //   System.out.println("resultado" + ciemp);
+        // System.out.println("Respuesta:\n" + (Math.sqrt(res)));
 
-      // String dia_semana = System.console().readLine();
-      // switch(dia_semana){
-      //   case "lunes":
-      //     System.out.println("gestão da qualidade ");
-      //     break;
-      //   case "martes":
-      //     System.out.println("Edu cacion fisica ");
-      //     break;
-      //   case "miercoles":
-      //     System.out.println("Artes ");
-      //     break;
-      //   case "jueves":
-      //     System.out.println("Portuges ");
-      //     break;
-      //   case "viernes":
-      //     System.out.println("Matematica");
-      //     break;
-      //   default:
-      //     System.out.println("Nao e um dia da semana");
-      //     break;
-      //   }
-      // Scanner scan = new Scanner(System.in );
-      // int hrs = scan.nextInt();
-      // if(hrs  >= 6 && hrs <= 12){
-      //   System.out.println ("Buenos dias");
-      //  }else if(hrs >= 13 && hrs <= 20){
-      //   System.out.println("Buenas tardes");
-      //  }else if(hrs >= 21 && hrs <= 5){
-      //   System.out.println("Buenos noches");
-      //  }else{
-      //   System.out.println("horario errado")       
-      //     }
-      // Scanner scan = new Scanner(System.in );
-      // System.out.println("pon algun numero");
-      // int dia_semana = scan.nextInt();
-      // switch(dia_semana){
-      //   case 1:
-      //     System.out.println("lunes");
-      //     break;
-      //   case 2:
-      //     System.out.println("martes");
-      //     break;
-      //   case 3:
-      //     System.out.println("miercoles ");
-      //     break;
-      //   case 4:
-      //     System.out.println("jueves");
-      //     break;
-      //   case 5:
-      //     System.out.println("viernes");
-      //     break;
-      //   case 6:
-      //     System.out.println("Sábado");
-      //     break;
-      //   case 7:
-      //     System.out.println("Domingos");
-      //     break;
-      //   default:
-      //     System.out.println("Coño");
-      //     break;
-      // }
-      //  Scanner scan = new Scanner(System.in );
-      //  System.out.println("pon algun tus horas trabajadas");
-      //  int hrsTrabajadas = scan.nextInt();
-      // if(hrsTrabajadas >= 41){
-      //   int horasExtras = hrsTrabajadas - 40;
-      //   System.out.println("Tu salário con horas extra: " + (horasExtras * 16 + 40 * 12));
-      // }else{
-      //   System.out.println("Tu salario es de: " +(hrsTrabajadas * 12));
-      // }
-      
-      // Scanner scan = new Scanner(System.in );
-      // System.out.println("primeiro numero??");
-      // int nt1 = scan.nextInt();
-      // System.out.println("Segundo numero??");
-      // int nt2 = scan.nextInt();
-      // System.out.println("tercer numero??");
-      // int nt3 = scan.nextInt();
-      // int resultado = nt1 + nt2 +nt3;
-      // System.out.println("resultado: " + resultado);
-      // System.out.println("RESULTADO FINAL:\n" + (resultado / 3));
-      
 
-      
 
-      
+        // Scanner scan = new Scanner(System.in );
+        // System.out.println("primeiro numero??");
+        // int nt1 = scan.nextInt();
+        // System.out.println("Segundo numero??");
+        // int nt2 = scan.nextInt();
+        // System.out.println("tercer numero??");
+        // int nt3 = scan.nextInt();
+        // int resultado = nt1 + nt2 +nt3;
+        // System.out.println("resultado: " + resultado);
+        // System.out.println("RESULTADO FINAL:\n" + (resultado / 3));
     }
 
     public static void main(String[] args) {

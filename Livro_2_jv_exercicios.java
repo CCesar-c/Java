78e211c6bf929454e009d73d54a51bf95ab58914
CCesar-c <cesarinfluencer14@gmail.com>
2026-06-24 14:57:21 -0300

    sim

--- Livro_2_jv_exercicios.java
+++ Livro_2_jv_exercicios.java
@@ -2492,11 +2492,26 @@ public class Livro_2_jv_exercicios {
     //
     //
     //
-    for (int in = 0; in < 10; in++) {
-      for (int i = 0; i <= 10; i++) {
-        System.out.println(i);
+
+      for (int x = 0; x <= 10; x++) {
+        for (int y = 0; y <= 1; y++) {
+          if(){
+            if(){
+              
+            }else{
+              
+            }
+          }
+          if(){
+            if(){
+              
+            }else{
+            
+            }
+          }
+        }
       }
-    }
+
     //
     //
     //
